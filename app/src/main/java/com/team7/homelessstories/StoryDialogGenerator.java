b2e80164ac6c731c70573e6f8d53c6c71a303c87
Austin Hsieh <austin.hsieh0@gmail.com>
2019-04-30 21:10:35 -0700

    add fonts to style and apply to story dialog, tweak Twigg story JSON

--- app/src/main/java/com/team7/homelessstories/StoryDialogGenerator.java
+++ app/src/main/java/com/team7/homelessstories/StoryDialogGenerator.java
@@ -1,14 +1,17 @@
 package com.team7.homelessstories;
 
 import android.content.DialogInterface;
+import android.graphics.Typeface;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 
 public class StoryDialogGenerator {
-    public static void showDialog(final FragmentActivity activity, final View container, final Story story){
+    public static void showDialog(final FragmentActivity activity, final View container, final Story story) {
         DialogInterface.OnClickListener buttonListener = new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
@@ -18,11 +21,17 @@ public class StoryDialogGenerator {
             }
         };
 
-        new AlertDialog.Builder(container.getContext())
+        AlertDialog dialog = new AlertDialog.Builder(container.getContext())
                 .setTitle("Read " + story.getName() + "'s Story")
                 .setMessage(story.getPreview())
                 .setPositiveButton("Read", buttonListener)
                 .setNegativeButton("Cancel", null)
-                .create().show();
+                .create();
+
+        dialog.show();
+
+        TextView messageText = dialog.findViewById(android.R.id.message);
+        Typeface face = ResourcesCompat.getFont(activity, R.font.open_sans_regular);
+        messageText.setTypeface(face);
     }
 }
