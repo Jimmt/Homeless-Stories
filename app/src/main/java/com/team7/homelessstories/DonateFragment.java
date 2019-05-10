71a05e167f4b9815c063cef1e333072696dd0d9e
Austin Hsieh <austin.hsieh0@gmail.com>
2019-05-09 20:52:42 -0700

    add donate screen, improve story card

--- app/src/main/java/com/team7/homelessstories/DonateFragment.java
+++ app/src/main/java/com/team7/homelessstories/DonateFragment.java
@@ -1,12 +1,21 @@
 package com.team7.homelessstories;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.material.card.MaterialCardView;
+
+import java.io.InputStream;
 
 import androidx.fragment.app.Fragment;
 
@@ -36,6 +45,47 @@ public class DonateFragment extends ListenerFragment {
         super.onCreateView(inflater, container, savedInstanceState);
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_donate, container, false);
+
+        String result = "";
+        try {
+            Resources res = getResources();
+            InputStream inputStream = res.openRawResource(R.raw.donate);
+
+            byte[] b = new byte[inputStream.available()];
+            inputStream.read(b);
+            result = new String(b);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String[] places = result.split("\n\\s*\n");
+
+        LinearLayout cardContainer = view.findViewById(R.id.donate_card_container);
+
+        for(int i = 0; i < places.length; i++){
+            String[] lines = places[i].split("\\n");
+            String image = lines[0].trim();
+            int imageId = getResources().getIdentifier(image, "drawable", getContext().getPackageName());
+            String name = lines[1];
+            String type = lines[2];
+            final String link = lines[3];
+
+            System.out.println(imageId + " " + image + R.drawable.avatar);
+            inflater.inflate(R.layout.donate_place_card, cardContainer);
+
+            MaterialCardView mcv = (MaterialCardView) cardContainer.getChildAt(i);
+            ((ImageView) mcv.findViewById(R.id.place_image)).setImageResource(imageId);
+            ((TextView) mcv.findViewById(R.id.place_name)).setText(name);
+            ((TextView) mcv.findViewById(R.id.place_type)).setText(type);
+
+            mcv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
+                    startActivity(browserIntent);
+                }
+            });
+        }
 
         listener.setToolbarStyle(true);
         return view;
